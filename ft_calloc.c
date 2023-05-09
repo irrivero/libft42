e016c347de4006df97ee8763982330bb8e27ca88
irrivero <116060502+irrivero@users.noreply.github.com>
2023-05-09 14:49:22 +0000

    part 1 kinda finished(a lot still to do, irene, remember please)

---
+++ ft_calloc.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 09:36:46 by irivero-          #+#    #+#             */
+/*   Updated: 2023/05/09 14:38:33 by irivero-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*p;
+
+	p = s;
+	while (n--)
+		*p++ = (unsigned char)c;
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	sbyte;
+	void	*p;
+	
+	sbyte = nmemb * size;
+	p = malloc(sbyte);
+	if (p != NULL)
+	{
+		ft_memset(p, 0, sbyte);
+	}
+	return (p);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main()
+{
+	int	i;
+	int	n;
+	int	*a;
+	
+	printf("Number of elements to be entered: ");
+	scanf("%d", &n);
+	a = (int *)ft_calloc(n, sizeof(int));
+	printf("Enter %i numbers: \n", n);
+	i = 0;
+	while (i < n)
+	{
+		scanf("%d", &a[i]);
+		i++;
+	}
+	printf("The numbers entered are: ");
+	i = 0;
+	while (i < n)
+	{
+		printf("%d ", a[i]);
+		i++;
+	}
+	printf("\n");
+	free(a);
+	return(0);
+}*/
--- libft.h
+++ libft.h
@@ -6,42 +6,43 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 14:44:00 by irivero-          #+#    #+#             */
-/*   Updated: 2023/05/09 09:29:12 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/05/09 14:15:28 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int	ft_atoi(const char *nptr)
-int	ft_isalnum(int c)
-int	ft_isalpha(int c)
-int	ft_isascii(int c)
-int	ft_isdigit(int c)
-int	ft_isprint(int c)
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-int	ft_tolower(int c)
-int	ft_toupper(int c)
-
-
-void	*ft_memchr(const void *s, int c, size_t n)
-void	bzero(void *s, size_t n)
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-void	*ft_memmove(void *dest, const void *src, size_t n)
-void	*ft_memset(void *s, int c, size_t n)
-
-char	*ft_strchr(const char *s, int c)
-char	*ft_strcpy(char	*dest, const char *src)
-char	*ft_strnstr(const char *big, const char *little, size_t len)
-char	*ft_strrchr(const char *s, int c)
+int	ft_atoi(const char *nptr);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 
-size_t	ft_strlen(const char *s)
+void	*ft_memchr(const void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strcpy(char	*dest, const char *src);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+
+size_t	ft_strlen(const char *s);
 
 
 #endif
