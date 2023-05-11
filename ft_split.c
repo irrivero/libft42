6b76093066f3c1083506f2c246c5c0425427bb79
irrivero <116060502+irrivero@users.noreply.github.com>
2023-05-11 13:26:27 +0000

    second part finished but it needs some second eye i feel like.

--- ft_split.c
+++ ft_split.c
@@ -6,23 +6,95 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:36:24 by irivero-          #+#    #+#             */
-/*   Updated: 2023/05/10 15:30:45 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/05/11 10:13:16 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_delim(char c, const char *set) //comprueba si c esta en set
+static int	count_words(char c, const char *s) //cuenta las substrings al dividir s por c
 {
-	while(*set)
+	int	mas;
+	
+	while(*s)
 	{
-		if (c == *set++)
-			return (0);
+		if (*s != c)
+		{
+			mas++;
+			while(*s && *s != c)
+				s++;
+		}
+		else
+			s++;
 	}
-	return (1);
+	return (mas);
+}
+
+static char	*copy(const char *s, char c) //copiar cada substring en su propia ubi de mem
+{
+	int		i;
+	char	*word;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	word = (char *) malloc(sizeof(char) * (i + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (*s && *s != c)
+		word[i++] = *s++;
+	word[i] = '\0';
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**split;
+	int		words;
+	int		i;
+
+	i = 0;
+	words = count_words(c, s); // se recorre s y se cuenta cuantas veces c
+	if (!s)
+		return (NULL);
+	split = (char **)malloc(sizeof(char *) * (words + 1)); // se reserva la mem para el array con todas las subcadenas y la cadena
+	if (!split)
+		return (NULL);
+	while (i < words) // se recorre la ch buscando c, cada c, se crea una subcadena y se sigue buscando
+	{
+		while (*s == c)
+			s++;
+		split[i] = copy(s, c);
+		if (!split[i])
+		{
+			while (i--)
+				free(split[i]);
+			free(split);
+			return (NULL);
+		}
+		while (*s && *s != c)
+			s++;
+		i++;
+	}
+	split[i] = NULL; // se agrega el null al final
+	return (split); //se devuelve el pointer
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "Fernandito,kit,kat\nkitkat kitkat";
+    char delim = ',';
+    char **result = ft_split(str, delim);
+    int i = 0;
+
+    while (result[i] != NULL)
+    {
+        printf("%s\n", result[i]);
+        i++;
+    }
+
+    return 0;
+}*/
