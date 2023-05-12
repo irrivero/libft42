4a83d2b761d7d0487684adff3999e7806db661c0
irrivero <116060502+irrivero@users.noreply.github.com>
2023-05-12 14:21:34 +0000

    poner un poco mas bonita la libreria

--- libft.h
+++ libft.h
@@ -6,17 +6,16 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 14:44:00 by irivero-          #+#    #+#             */
-/*   Updated: 2023/05/12 10:50:18 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/05/12 14:21:02 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
+# include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
 typedef struct		s_list
 {
 	void			*content;
@@ -27,19 +26,18 @@ t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
-int	ft_atoi(const char *nptr);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_atoi(const char *nptr);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
-int	ft_lstsize(t_list *lst);
-
+int		ft_lstsize(t_list *lst);
 
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -53,6 +51,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
